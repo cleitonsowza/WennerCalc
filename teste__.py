b808108038921b5f2db0bdc1b4b38c8406c00c54
Cleiton Cunha <85006663+cleitonsowza@users.noreply.github.com>
2024-11-15 11:24:48 -0300

    Add files via upload

--- teste__.py
+++ teste__.py
@@ -88,9 +88,10 @@ def codigo_formiga(dados__: dict):
 
 
     def cxTwoPointCopy(ind1, ind2):
-        """Execute a two points crossover with copy on the input individuals. The
-        copy is required because the slicing in numpy returns a view of the data,
-        which leads to a self overwriting in the swap operation. It prevents
+        """Execute um cruzamento de dois pontos com cópia nos elementos de
+        entrada. A cópia é necessária porque o fatiamento em numpy retorna uma
+        visão dos dados, o que leva a uma sobrescrição de si mesmo na operação
+        de troca. Isso previne
         ::
 
             >>> import numpy
@@ -108,7 +109,7 @@ def codigo_formiga(dados__: dict):
         cxpoint2 = random.randint(1, size - 1)
         if cxpoint2 >= cxpoint1:
             cxpoint2 += 1
-        else: # Swap the two cx points
+        else: #
             cxpoint1, cxpoint2 = cxpoint2, cxpoint1
 
         ind1[cxpoint1:cxpoint2], ind2[cxpoint1:cxpoint2] \
@@ -127,15 +128,15 @@ def codigo_formiga(dados__: dict):
             halloffame=None, 
             verbose=__debug__,
     ):
-        """This algorithm is similar to DEAP eaSimple() algorithm, with the modification that
-        halloffame is used to implement an elitism mechanism. The individuals contained in the
-        halloffame are directly injected into the next generation and are not subject to the
-        genetic operators of selection, crossover and mutation.
-        """
+        """Este algoritmo é semelhante ao algoritmo eaSimple() do DEAP, com a modificação de que
+        o halloffame é usado para implementar um mecanismo de elitismo. Os indivíduos contidos
+        no halloffame são diretamente injetados na próxima geração e não estão sujeitos aos 
+        operadores genéticos de seleção, crossover e mutação."""
+
         logbook = tools.Logbook()
         logbook.header = ['gen', 'nevals'] + (stats.fields if stats else [])
 
-        # Evaluate the individuals with an invalid fitness
+        #
         invalid_ind = [ind for ind in population if not ind.fitness.valid]
         fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
         for ind, fit in zip(invalid_ind, fitnesses):
@@ -156,28 +157,28 @@ def codigo_formiga(dados__: dict):
         # Begin the generational process
         for gen in range(1, ngen + 1):
 
-            # Select the next generation individuals
+            #
             offspring = toolbox.select(population, len(population) - hof_size)
 
-            # Vary the pool of individuals
+            #
             offspring = algorithms.varAnd(offspring, toolbox, cxpb, mutpb)
 
-            # Evaluate the individuals with an invalid fitness
+            #
             invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
             fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
             for ind, fit in zip(invalid_ind, fitnesses):
                 ind.fitness.values = fit
 
-            # add the best back to population:
+            #
             offspring.extend(halloffame.items)
 
-            # Update the hall of fame with the generated individuals
+            #
             halloffame.update(offspring)
 
-            # Replace the current population by the offspring
+            #
             population[:] = offspring
 
-            # Append the current generation statistics to the logbook
+            #
             record = stats.compile(population) if stats else {}
             logbook.record(gen=gen, nevals=len(invalid_ind), **record)
             if verbose:
